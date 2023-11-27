8f3417128a6a76baefbca4482e76a8964f0c7313
Twan van Noorloos <twan.v.n@outlook.com>
2023-11-27 18:15:49 +0000

    Added simple connection status to dashboard

---
+++ javascript/basic_ros_connection.js
@@ -0,0 +1,29 @@
+// Connecting to ROS
+// -----------------
+var ros = new ROSLIB.Ros();
+
+// If there is an error on the backend, an 'error' emit will be emitted.
+ros.on('error', function (error) {
+    let span = document.getElementById("connection_tag");
+    span.textContent = "Error";
+    span.style.color = 'yellow';
+    console.log(error);
+});
+
+// Find out exactly when we made a connection.
+ros.on('connection', function () {
+    let span = document.getElementById("connection_tag");
+    span.textContent = "Connected";
+    span.style.color = 'green';
+    console.log('Connection made!');
+});
+
+ros.on('close', function () {
+    let span = document.getElementById("connection_tag");
+    span.textContent = "No connection";
+    span.style.color = 'red';
+    console.log('Connection closed.');
+});
+
+// Create a connection to the rosbridge WebSocket server.
+ros.connect('ws://' + location.hostname + ':9090', 'echo-protocol');
