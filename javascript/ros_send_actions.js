91c328988fc0abdd0fd9cdec25c06f91e8fc9bba
Twan van Noorloos <twan.v.n@outlook.com>
2024-01-13 14:44:32 +0000

    Added code for sending value to one axis

--- javascript/ros_send_actions.js
+++ javascript/ros_send_actions.js
@@ -1,12 +1,21 @@
 const forms = document.querySelectorAll('form');
 const form = forms[0];
 
+const mylist = document.getElementById("myList");
+
 document.getElementById("position-submit").addEventListener("click", sendNewTargetPosition);
+document.getElementById("axis-submit").addEventListener("click", sendNewAxisValue);
 
 var targetPositionTopic = new ROSLIB.Topic({
     ros: ros,
     name: '/target_position',
     messageType: 'std_msgs/Int16MultiArray'
+});
+
+var axisPositionTopic = new ROSLIB.Topic({
+    ros: ros,
+    name: '/axis_target',
+    messageType: 'std_msgs/String'
 });
 
 function sendNewTargetPosition() {
@@ -27,4 +36,21 @@ function sendNewTargetPosition() {
 
     targetPositionTopic.publish(position);
     console.log("Yas");
+}
+
+function sendNewAxisValue() {
+    let axis = mylist.options[mylist.selectedIndex].text;
+
+    if (axis !== "---Choose axis---") {
+        const messageValue = axis + " " + document.getElementById('target-axisValue').value;
+        console.log(messageValue);
+
+        var axisValue = new ROSLIB.Message({
+            data: messageValue
+        });
+
+        axisPositionTopic.publish(axisValue);
+    } else {
+        console.log("No actual axis");
+    }
 }
